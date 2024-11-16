6884e3ff879aea649d10b33e269bd98d6f297ae4
Mark van Eijk <mark@markjuh.net>
2024-11-16 11:15:13 +0100

    Update version for updated dependencies.

--- cmds/datedir/cmd.go
+++ cmds/datedir/cmd.go
@@ -13,7 +13,7 @@ import (
 
 var Cmd = &bonzai.Cmd{
   Name: `datedir`,
-  Vers: `v0.0.2`,
+  Vers: `v0.0.3`,
   Short: `Make, remove and switch to directory for current date`,
   Alias: `d`,
   Long: `
